f96ad444df54e76fd46993ba61a5b750117fd3e2
hadikhalil19 <hadikhalil19@gmail.com>
2023-07-27 14:49:53 +1000

    Dialogue SO and Node and Editor created

---
+++ Assets/Scripts/Dialogue/DailogueNode.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DailogueNode 
+{
+    public string uniqueID;
+    public string text;
+    public string[] children;
+}
