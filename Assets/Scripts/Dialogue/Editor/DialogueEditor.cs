4d81575f5e7c334019948e6e7a195b69b4ceec3e
hadikhalil19 <hadikhalil19@gmail.com>
2023-07-31 15:28:36 +1000

    Dailogue Editor Window created with Hello World

--- Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 namespace Proto.Dialogue.Editor {
@@ -9,12 +10,22 @@ namespace Proto.Dialogue.Editor {
         [MenuItem("Window/Dialogue Editor")]
         private static void ShowEditorWindow() {
             var window = GetWindow<DialogueEditor>();
-            window.titleContent = new GUIContent("Dialogue");
+            window.titleContent = new GUIContent("Dialogue Editor");
             window.Show();
         }
 
         private void OnGUI() {
+            EditorGUI.LabelField(new Rect(10,10,200,200), "Hello World");
+        }
+
+        [OnOpenAssetAttribute(1)]
+        public static bool OnOpenDialogueAsset(int instanceID, int line) {
             
+           if(EditorUtility.InstanceIDToObject(instanceID) is Dialogue dialogue){
+                ShowEditorWindow();
+                return true;
+            }
+            return false;
         }
     }
 }
