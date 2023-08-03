88f9864461791895cba78f69eb4cca6ae4ddcad1
hadikhalil19 <hadikhalil19@gmail.com>
2023-08-03 15:00:14 +1000

    Added Dictionary for Child node lookup and implmented connections to child nodes using Bezier curves.

--- Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -59,7 +59,11 @@ namespace Proto.Dialogue.Editor {
                 ProcessEvents();
                 foreach (DialogueNode node in selectedDialogue.GetAllNodes())
                 {
-                    OnGUINode(node);
+                    DrawConnections(node);
+                }
+                foreach (DialogueNode node in selectedDialogue.GetAllNodes())
+                {
+                    DrawNode(node);
                 }
             }
             
@@ -87,7 +91,7 @@ namespace Proto.Dialogue.Editor {
             }
         }
 
-        private void OnGUINode(DialogueNode node)
+        private void DrawNode(DialogueNode node)
         {
             GUILayout.BeginArea(node.rect, nodeStyle);
             EditorGUI.BeginChangeCheck();
@@ -106,6 +110,22 @@ namespace Proto.Dialogue.Editor {
             
             GUILayout.EndArea();
         }
+
+         private void DrawConnections(DialogueNode node) {
+            Vector3 startPosition = new Vector2(node.rect.xMax, node.rect.center.y);
+            foreach (DialogueNode childNode in selectedDialogue.GetAllChildren(node))
+            {
+                Vector3 endPosition = new Vector2(childNode.rect.xMin, childNode.rect.center.y);
+                Vector3 controlPointOffset = endPosition - startPosition;
+                controlPointOffset.y = 0;
+                controlPointOffset.x *= 0.6f;
+                Handles.DrawBezier(
+                    startPosition, endPosition, 
+                    startPosition + controlPointOffset, 
+                    endPosition - controlPointOffset, 
+                    Color.white, null, 4f);
+            }
+         }
 
         private DialogueNode GetNodeAtPoint(Vector2 point)
         {
