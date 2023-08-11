1d42b7ec2654b470b1aed41a991ea3e882439b3a
hadikhalil19 <hadikhalil19@gmail.com>
2023-08-11 14:24:04 +1000

    Dialogue Architecture created with DialogueUI and PlayerConversant scripts to hook to get the text from the first node of the dialogue SO provided from the inspector.

---
+++ Assets/Scripts/Scene Management/UI Scripts/DialogueUI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Proto.Dialogue;
+using TMPro;
+
+namespace Proto.SceneManagement.UI {
+    public class DialogueUI : MonoBehaviour
+    {
+        PlayerConversant playerConversant;
+        [SerializeField] TextMeshProUGUI AIText;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            playerConversant = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerConversant>();
+            AIText.text = playerConversant.GetText();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
