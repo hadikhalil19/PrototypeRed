89222638308a74daeca17c6cf357f4ea8a47522e
hadikhalil19 <hadikhalil19@gmail.com>
2023-08-29 14:20:05 +1000

    Player death changed to no longer destroy the core player object but disable contols and reload camp scene

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -10,6 +10,7 @@ public class PlayerHealth : Singleton<PlayerHealth>
     [SerializeField] private int maxHealth = 3;
     [SerializeField] private float knockBackThrustAmount = 5f;
     [SerializeField] private float damageRecoveryTime = 0.5f;
+    [SerializeField] private string deathSceneTransitionName;
 
     private int currentHealth;
     private bool canTakeDamage =  true;
@@ -19,7 +20,7 @@ public class PlayerHealth : Singleton<PlayerHealth>
     const string HEALTH_SLIDER_TEXT = "Health Slider";
     const string CAMP_TEXT = "Camp";
     readonly int DEATH_HASH = Animator.StringToHash("Death");
-    //readonly int RELOAD_HASH = Animator.StringToHash("Reload");
+    readonly int RELOAD_HASH = Animator.StringToHash("Reload");
 
 
 
@@ -66,7 +67,8 @@ public class PlayerHealth : Singleton<PlayerHealth>
     private void CheckIfPlayerDeath() {
         if (currentHealth <= 0 && !IsDead) {
             IsDead = true;
-            Destroy(ActiveWeapon.Instance.gameObject);
+            //Destroy(ActiveWeapon.Instance.gameObject);
+            ActiveWeapon.Instance.disableAttack = true;
             currentHealth = 0;
             GetComponent<Animator>().SetTrigger(DEATH_HASH);
             StartCoroutine(DeathLoadSceneRoutine());
@@ -74,12 +76,23 @@ public class PlayerHealth : Singleton<PlayerHealth>
     }
 
     private IEnumerator DeathLoadSceneRoutine() {
-        yield return new WaitForSeconds(3f);
-        Destroy(gameObject);
-        //GetComponent<Animator>().SetTrigger(RELOAD_HASH);
-        //currentHealth = maxHealth;
-        //UpdateHealthSlider();
+        yield return new WaitForSeconds(1f);
+        //Destroy(gameObject);
+        //SceneManager.LoadScene(CAMP_TEXT);
+        SceneManagement.Instance.SetTransitionName(deathSceneTransitionName);
+        UIFade.Instance.FadeIn();
+        StartCoroutine(LoadSceneRoutine());
+    }
+
+    private IEnumerator LoadSceneRoutine() {
+        yield return  new WaitForSeconds(3f);
+        currentHealth = maxHealth;
+        UpdateHealthSlider();
+        IsDead = false;
         SceneManager.LoadScene(CAMP_TEXT);
+        
+        ActiveWeapon.Instance.disableAttack = false; 
+        GetComponent<Animator>().SetTrigger(RELOAD_HASH);   
     }
 
 
